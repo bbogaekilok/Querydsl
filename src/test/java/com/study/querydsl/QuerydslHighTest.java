33a0b8a4118e687dc3372a85ecdce3bd65c44478
bbogaekilok <bbogaekilok@gmail.com>
2024-04-15 13:52:27 +0900

    프로젝션 결과 반환(기본/DTO조회/Q파일 생성)

--- src/test/java/com/study/querydsl/QuerydslHighTest.java
+++ src/test/java/com/study/querydsl/QuerydslHighTest.java
@@ -1,7 +1,12 @@
 package com.study.querydsl;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.dto.MemberDto;
+import com.study.querydsl.dto.QMemberDto;
 import com.study.querydsl.entity.Member;
 import com.study.querydsl.entity.QMember;
 import com.study.querydsl.entity.Team;
@@ -77,6 +82,125 @@ public class QuerydslHighTest {
             Integer age = tuple.get(member.age);
             System.out.println("username : " + username);
             System.out.println("age : " + age);
+        }
+    }
+
+
+    /**
+     * 프로젝션 결과를 Dto로 반환하기
+     * 순수 JPA 에서 Dto를 조회할 때 new 명령어를 사용함.
+     * 경로를 다 적어줘야한다는 단점이 있으며
+     * 생성자 방식만을 지원한다.
+     */
+    @Test
+    public void findDtoByJPQL() {
+        List<MemberDto> result = em.createQuery(
+                "select new study.querydsl.dto.MemberDto(m.username, m.age) from Member m", MemberDto.class)
+                .getResultList();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto :" + memberDto);
+        }
+    }
+
+    /**
+     * Querydsl로 Dto로 결과 반환하기 3가지 방법
+     * 1. 프로퍼티 접근 - setter
+     * Projections.bean() 를 활용
+     */
+    @Test
+    public void findDtoBySetter(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.bean(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = "+memberDto);
+        }
+    }
+
+
+    /**
+     * Querydsl로 Dto로 결과 반환하기 3가지 방법
+     * 2. 필드 직접 접근
+     * Projections.fields()
+     */
+    @Test
+    public void findDtoByField(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = "+memberDto);
+        }
+    }
+
+    /**
+     * Querydsl로 Dto로 결과 반환하기 3가지 방법
+     * 2. 필드 직접 접근
+     * 별치이 다를 때 .as() 사용
+     */
+    @Test
+    public void findUserDto(){
+        QMember memberSub = new QMember("memberSub");
+
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.username.as("name"), //이름이 안 맞을 땐 as를 이용하여 이름을 맞춰준다. 이름이 안 맞으면 매핑이 안됨.
+
+                        // 서브쿼리
+                        ExpressionUtils.as(JPAExpressions
+                                .select(memberSub.age.max())
+                                .from(memberSub), "age")
+                ))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = "+memberDto);
+        }
+    }
+
+
+    /**
+     * Querydsl로 Dto로 결과 반환하기 3가지 방법
+     * 3. 생성자 사용
+     * Projections.constructor()
+     */
+    @Test
+    public void findDtoByContructor(){
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = "+memberDto);
+        }
+    }
+
+    /**
+     * @QueryProjection
+     * Dto를 Q파일로 생성 함.
+     */
+    @Test
+    public void findDtoByQueryProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = "+memberDto);
         }
     }
 
